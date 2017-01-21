d38e26780f024a15be62e03499485cdf50d4e555
Екатерина <tuz.kat@gmail.com>
2017-01-21 23:29:40 +0300

    Рассчет расстояния между двумя точками на плоскости (задание 3)

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kate on 21.01.2017.
+ */
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
--- sandbox/src/main/java/myfirstpro.java
+++ sandbox/src/main/java/myfirstpro.java
@@ -1,5 +1,15 @@
-public class Myfirstpro {
-public static void main(String[] args){
-System.out.println("Hello World!");
-}
+//import java.math.MathContext;
+//import java.math.BigDecimal;
+public class myfirstpro {
+    public static void main(String[] args) {
+       Point first = new Point(15,27);
+        Point second = new Point(56,29);
+        System.out.println("Расстояние между двумя точками с координатами (" + first.x + "),(" + first.y + ") и (" + second.x + "," + second.y + ") равно " + distance(first,second));
+    }
+    public static double distance(Point first, Point second) {
+    double k, m;
+    k = Math.pow((first.x - second.x),2);
+    m = Math.pow((first.y - second.x),2);
+        return (Math.sqrt(k+m));
+    }
 }
