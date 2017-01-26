04879a17b415e7ffbe96aae252defdbdf48cd6f5
Екатерина <tuz.kat@gmail.com>
2017-01-26 22:42:22 +0300

    Рефакторинг. Группировка параметров в объекты GroupData

--- addressbook-web-tests/src/test/java/ru/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ContactCreationTests.java
@@ -1,3 +1,5 @@
+package ru;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -36,7 +38,7 @@ public class ContactCreationTests {
     @Test
     public void testContactCreation() {
         gotoAddContactPage();
-        fillContactForm("Nikita", "Valerievich", "Baliassniy", "Home", "nikita.baliassniy@gmail.com", "+79787397913");
+        fillContactForm(new ContactData("Nikita", "Valerievich", "Baliassniy", "Home", "nikita.baliassniy@gmail.com", "+79787397913"));
         enteringContactInfo();
         returnToHomePage();
     }
@@ -49,25 +51,25 @@ public class ContactCreationTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillContactForm(String firstname, String middlename, String lastname, String company, String address, String home_tel) {
+    private void fillContactForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(firstname);
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
         wd.findElement(By.name("middlename")).click();
         wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(middlename);
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(lastname);
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
         wd.findElement(By.name("company")).click();
         wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(company);
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(address);
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(home_tel);
+        wd.findElement(By.name("home")).sendKeys(contactData.getHome_tel());
     }
 
     private void gotoAddContactPage() {
---
+++ addressbook-web-tests/src/test/java/ru/ContactData.java
@@ -0,0 +1,43 @@
+package ru;
+
+public class ContactData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String company;
+    private final String address;
+    private final String home_tel;
+
+    public ContactData(String firstname, String middlename, String lastname, String company, String address, String home_tel) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.company = company;
+        this.address = address;
+        this.home_tel = home_tel;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHome_tel() {
+        return home_tel;
+    }
+}
