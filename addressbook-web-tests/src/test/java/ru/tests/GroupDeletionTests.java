b791f9161b78bb51e276ebb18aa13c63e50f7035
Екатерина <tuz.kat@gmail.com>
2017-02-01 22:11:03 +0300

    Обеспечение предусловий выполнения теста. Группы.
    Проверка, существует ли группа чтобы ее удалить или модифицировать. Если
    ни одной группы нет, то сначала создаем ее.

--- addressbook-web-tests/src/test/java/ru/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/tests/GroupDeletionTests.java
@@ -1,6 +1,8 @@
 package ru.tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import ru.models.GroupData;
 
 public class  GroupDeletionTests extends TestBase{
     
@@ -8,7 +10,12 @@ public class  GroupDeletionTests extends TestBase{
     
     @Test
     public void testgroupDeletion() {
+
         app.getNafigationHelper().gotoGroupPage();
+        //проверка существует ли группа для удаления. если нет - то создаем ее
+        if (!app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSeletedGroups();
         app.getGroupHelper().returnToGroupPage();
