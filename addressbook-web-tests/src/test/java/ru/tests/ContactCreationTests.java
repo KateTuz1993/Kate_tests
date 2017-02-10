84ea2ea5e358db6fdd95053f1a8eca1e6cced384
Екатерина <tuz.kat@gmail.com>
2017-02-10 19:31:56 +0300

    Добавление проверки в метод Создания контакта: есть ли хоть одна группа

--- addressbook-web-tests/src/test/java/ru/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.models.ContactData;
+import ru.models.GroupData;
 
 
 import java.util.List;
@@ -14,6 +15,13 @@ public class ContactCreationTests extends TestBase{
     @Test
     public void testContactCreation() {
         List<ContactData> before = app.getContactHelper().getContactList();
+        //проверка есть ли хоть одна группа? если нет, сначала создаем ее
+        app.getNafigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            //app.getNafigationHelper().gotoGroupPage();
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
+        app.getNafigationHelper().goToHomePage();
         app.getNafigationHelper().gotoAddContactPage();
         app.getContactHelper().createContact(new ContactData("Nikita", "Valerievich", "Baliassniy", "Home", "nikita.baliassniy@gmail.com", "+79787397913", "test1"),true);
         List<ContactData> after = app.getContactHelper().getContactList();
