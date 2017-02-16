0f9d29003bba4fca4a227f74a9901e25153e81e6
Екатерина <tuz.kat@gmail.com>
2017-02-16 14:12:19 +0300

    Модификация - к предыдущему коммиту

--- addressbook-web-tests/src/test/java/ru/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/tests/ContactModificationTests.java
@@ -5,8 +5,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.models.ContactData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class ContactModificationTests extends TestBase{
 
@@ -14,7 +13,7 @@ public class ContactModificationTests extends TestBase{
     public  void ensurePreconditions() { // проверка выполения предусловий
         //проверка существует ли контакт для модификаци. если нет - то создаем его
 
-        if(app.contact().list().size()==0){
+        if(app.contact().all().size()==0){
             app.goTo().addContactPage();
             app.contact().create(new ContactData().withFirstname("Nikita").withMiddlename("Valerievich").withLastname("Baliassniy").withCompany("Home").withAddress("nikita.baliassniy@gmail.com").withHome_tel("+79787397913").withGroup("test1"),true);
         }
@@ -23,23 +22,18 @@ public class ContactModificationTests extends TestBase{
     @Test //(enabled = false)
     public void testContactModification(){
 
-        List<ContactData> before = app.contact().list();
+        Set<ContactData> before = app.contact().all();
+        ContactData modifiedContact = before.iterator().next(); //выбираем первый попавшийся контакт
         int index = before.size()-1;
-        ContactData contact = new ContactData().withId(before.get(index).getId()).withFirstname("Nikita").withMiddlename("Valerievich").withLastname("Baliassniy").withCompany("Home").withAddress("nikita.baliassniy@gmail.com").withHome_tel("+79787397913");
+        ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirstname("Nikita2").withMiddlename("Valerievich2").withLastname("Baliassniy2").withCompany("Home").withAddress("nikita.baliassniy@gmail.com").withHome_tel("+79787397913");
 
-        app.contact().modify(index, contact);
-        List<ContactData> after = app.contact().list();
+        app.contact().modify(contact);
+        Set<ContactData> after = app.contact().all();
         Assert.assertEquals(after.size(),before.size());
 
         //сравнение множеств контактов до и после модификации
-        before.remove(index); //изменяем список контактов: удаляем модифицируемый и добавляем его же с новыми значениями
+        before.remove(modifiedContact); //изменяем список контактов: удаляем модифицируемый и добавляем его же с новыми значениями
         before.add(contact);
-        //сортировка списков по Id
-        Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
-        before.sort(byId);
-        after.sort(byId);
-
-
         Assert.assertEquals(before , after); //сравнение списков контактов на странице
     }
 
