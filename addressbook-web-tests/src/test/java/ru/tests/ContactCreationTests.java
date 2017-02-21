189cdab78ba3d8d246ccd3ddcd361f9a8fa0bcad
Екатерина <tuz.kat@gmail.com>
2017-02-21 22:10:47 +0300

    Параметризация методов создания групп и контактов
    Добавлен провайдер тестовых данных @DataProvider

--- addressbook-web-tests/src/test/java/ru/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/tests/ContactCreationTests.java
@@ -1,19 +1,32 @@
 package ru.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.models.ContactData;
 import ru.models.Contacts;
 import ru.models.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-    @Test //(enabled = false)
-    public void testContactCreation() {
+    @DataProvider //провайдер тестовых данных
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new ContactData().withFirstname("Nikita").withMiddlename("Valerievich").withLastname("Baliassniy").withCompany("Home").withAddress("Хрусталева 97,91").withGroup("[none]").withHomePhone("32-11-58").withMobilePhone("+79787397913").withEmail("nikita.baliassniy@gmail.com").withEmail2("n.b@gmail.com")});
+        list.add(new Object[]{new ContactData().withFirstname("Valentina").withMiddlename("Valentinovna").withLastname("Okuneva").withCompany("Home").withAddress("Хрусталева 97,91").withGroup("[none]").withHomePhone("32-11-58").withMobilePhone("+79787397913").withEmail("nikita.baliassniy@gmail.com").withEmail2("n.b@gmail.com")});
+        list.add(new Object[]{new ContactData().withFirstname("Tuzhilkin").withMiddlename("Leonidovich").withLastname("Alexander").withCompany("Home").withAddress("Хрусталева 97,91").withGroup("[none]").withHomePhone("32-11-58").withMobilePhone("+79787397913").withEmail("nikita.baliassniy@gmail.com").withEmail2("n.b@gmail.com")});
+        return list.iterator();
+    }
+
+    @Test (dataProvider = "validContacts")//(enabled = false)
+    public void testContactCreation(ContactData contact) {
         Contacts before = app.contact().all();
         //проверка есть ли хоть одна группа? если нет, сначала создаем ее
         app.goTo().groupPage();
@@ -23,7 +36,7 @@ public class ContactCreationTests extends TestBase{
         app.goTo().goToHomePage();
         app.goTo().addContactPage();
         File photo = new File("src/2.PNG");
-        ContactData contact = new ContactData().withFirstname("Nikita").withMiddlename("Valerievich").withLastname("Baliassniy").withCompany("Home").withAddress("Хрусталева 97,91").withGroup("[none]").withHomePhone("+79787397913").withEmail("nikita.baliassniy@gmail.com").withEmail2("dfsf").withPhoto(photo);
+        //ContactData contact = new ContactData().withFirstname("Nikita").withMiddlename("Valerievich").withLastname("Baliassniy").withCompany("Home").withAddress("Хрусталева 97,91").withGroup("[none]").withHomePhone("+79787397913").withEmail("nikita.baliassniy@gmail.com").withEmail2("dfsf").withPhoto(photo);
         app.contact().create(contact,true);
         assertThat(app.contact().count(),equalTo(before.size() + 1)); //сравниваем кол-во контактов до начала теста добавления - и после
         Contacts after = app.contact().all();
