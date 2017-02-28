c7825a13541291aa3703957f37631345cebf3acc
Екатерина <tuz.kat@gmail.com>
2017-02-28 21:45:58 +0300

    Сравнение данных загруженных из БД. Часть 1.
    DbHelper

--- addressbook-web-tests/src/test/java/ru/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
     private NafigationHelper nafigationHelper;
     private GroupHelper groupHelper;
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -32,6 +33,8 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
 
         if (Objects.equals(browser, BrowserType.FIREFOX)){
             wd = new FirefoxDriver();
@@ -49,6 +52,7 @@ public class ApplicationManager {
         contactHelper = new ContactHelper(wd);
         sessionHelper = new SessionHelper(wd);
         sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
     }
 
         public void stop() {
@@ -64,4 +68,6 @@ public class ApplicationManager {
     public ContactHelper contact() {
         return contactHelper;
     }
+
+    public DbHelper db() { return dbHelper; }
 }
