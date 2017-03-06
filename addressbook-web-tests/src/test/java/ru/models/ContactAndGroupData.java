6146e0c66540552dd00793c400fe3cf6f493981a
Mikhailyuk.Nick <mihayluk.nikolay@otr.ru>
2017-03-06 17:52:56 +0300

    Тест добавления контакта в группу. Часть 3.1.

--- addressbook-web-tests/src/test/java/ru/models/ContactAndGroupData.java
+++ addressbook-web-tests/src/test/java/ru/models/ContactAndGroupData.java
@@ -15,7 +15,7 @@ public class ContactAndGroupData {
     private int contactId = Integer.MAX_VALUE;
 
     @Column(name = "group_id") //это id группы
-    private int groupId = Integer.MIN_VALUE;
+    private int groupId = Integer.MAX_VALUE;
 
     public int getContactId() {
         return contactId;
@@ -23,6 +23,33 @@ public class ContactAndGroupData {
 
     public int getGroupId() {
         return groupId;
+    }
+
+    public ContactAndGroupData withId(int id) {
+        this.contactId = id;
+        return this;
+    }
+    public ContactAndGroupData withGroupId(int groupId) {
+        this.groupId = groupId;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactAndGroupData that = (ContactAndGroupData) o;
+
+        if (contactId != that.contactId) return false;
+        return groupId == that.groupId;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = contactId;
+        result = 31 * result + groupId;
+        return result;
     }
 
     @Override
