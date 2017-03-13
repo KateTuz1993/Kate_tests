9a6367c71376933d6ca7b2909a386904793c1054
Екатерина <tuz.kat@gmail.com>
2017-03-13 17:33:37 +0300

    Ленивая инициализация браузера
    Теперь доступ к браузеру через getDriver

--- mantis-tests/src/test/java/ru/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/appmanager/HttpSession.java
@@ -38,10 +38,7 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params)); //устанавливаем параметры запроса post
         CloseableHttpResponse response = httpclient.execute(post); //отправка запроса
         String body = getTextFrom(response); //получаем текст ответа от сервера
-        //return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
-        JOptionPane.showMessageDialog(null, body);
         return body.contains(String.format("<span class=\"user-info\">%s</span>",username)); //проверка, действительно ли зашел пользователь
-        //return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
     }
 
     private String getTextFrom(CloseableHttpResponse response) throws IOException {
@@ -58,11 +55,7 @@ public class HttpSession {
         HttpGet get =  new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
         CloseableHttpResponse response = httpclient.execute(get);
         String body = getTextFrom(response);
-        //return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
         return body.contains(String.format("<span class=\"user-info\">%s</span>",username));
-        //return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
-
-
 
     }
 }
