60fe8819bfaa3fae76a8573dadddfbcd62af7910
Екатерина <tuz.kat@gmail.com>
2017-03-23 21:28:48 +0300

    Добавлена возможность выбора платформы запуска тестов

--- addressbook-web-tests/src/test/java/ru/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -50,9 +51,10 @@ public class ApplicationManager {
                 wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
             }
         }
-        else { // для удаленного запуска браузера
+        else { // для удаленного запуска тестов
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
