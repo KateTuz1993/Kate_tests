2094d9dacf3e216a1b350554b8cecd2dd8841ec5
Екатерина <tuz.kat@gmail.com>
2017-03-26 14:49:01 +0300

    Добавлена генерация Allure отчетов

--- addressbook-web-tests/src/test/java/ru/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/tests/TestBase.java
@@ -5,10 +5,8 @@ import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 import ru.models.ContactData;
@@ -21,6 +19,7 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+@Listeners(MyTestListener.class)
 
 public class TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class); //подключаем логирование
@@ -29,9 +28,9 @@ public class TestBase {
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite //один запуск браузера и выполенение нескольких тестов
-    public void setUp() throws Exception {
+    public void setUp(ITestContext context) throws Exception {
         app.init();
-
+        context.setAttribute("app", app);
     }
 
     @AfterSuite(alwaysRun = true)
